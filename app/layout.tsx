705aea637da1bb282147b4e48573e23066ec055b
Pranav Rao <pranavrao541@gmail.com>
2026-03-25 22:49:42 +0530

    update: add metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
-import { Geist, Geist_Mono, Outfit } from "next/font/google"
-
-import "./globals.css"
+import { Metadata } from "next"
+import { Geist_Mono, Outfit } from "next/font/google"
 import { ThemeProvider } from "@/components/theme-provider"
 import { cn } from "@/lib/utils"
+
+import "./globals.css"
 
 const outfit = Outfit({ subsets: ["latin"], variable: "--font-sans" })
 
@@ -10,6 +11,12 @@ const fontMono = Geist_Mono({
   subsets: ["latin"],
   variable: "--font-mono",
 })
+
+export const metadata: Metadata = {
+  title: "Job Preparation AI",
+  description:
+    "An AI-powered career assistant providing personalized mock interviews, resume feedback, and comprehensive tools to help you land your dream job.",
+}
 
 export default function RootLayout({
   children,
