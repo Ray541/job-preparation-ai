d7de790c5d255f3c096731e9b62b20b71af07dfa
Pranav Rao <pranavrao541@gmail.com>
2026-03-25 22:40:35 +0530

    update: change hotkey combination

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { Button } from "@/components/ui/button"
 
 export default function Page() {
   return (
-    <div className="flex min-h-svh p-6">
+    <div className="flex min-h-svh">
       <div className="flex max-w-md min-w-0 flex-col gap-4 text-sm leading-loose">
         <div>
           <h1 className="font-medium">Project ready!</h1>
@@ -11,7 +11,7 @@ export default function Page() {
           <Button className="mt-2">Button</Button>
         </div>
         <div className="font-mono text-xs text-muted-foreground">
-          (Press <kbd>d</kbd> to toggle dark mode)
+          (Press <kbd>ctl + alt + z</kbd> to toggle dark mode)
         </div>
       </div>
     </div>
